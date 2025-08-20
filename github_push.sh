b102f4f11859ae4b64fccd6c7f85deffab82fc5b
Johny-1992 <120253850+Johny-1992@users.noreply.github.com>
2025-08-20 19:08:54 +0200

    github_push.sh

--- github_push.sh
+++ github_push.sh
@@ -7,6 +7,6 @@ NAME=${1:-tgst-empire}
 curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$NAME\",\"private\":true}" >/dev/null
 git init && git add . && git commit -m "TGST Empire initial"
 git branch -M main
-USER=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user | jq -r .login)
+USER=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user | jq -r .login) 
 git remote add origin https://github.com/$USER/$NAME.git
 git push -u origin main
